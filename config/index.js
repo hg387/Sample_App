793e97afcebef4a762c4f5f8b852bdc7d4f44edb
hg387 <hg387@drexel.edu>
2021-10-25 01:29:39 -0400

    adding tests

--- config/index.js
+++ config/index.js
@@ -1,3 +1,3 @@
 const prod = process.env.NEXT_CHECK_ENV === "production"
 
-export const server = 'https://sample-app-git-hg387-hg387.vercel.app/'
+export const server = prod ? 'https://sample-app-pvphvjlix-hg387.vercel.app' : 'http://localhost:3000'
