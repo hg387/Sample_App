b6a6378715bf68bc461b897967a37b5ef15f7bc3
hg387 <hg387@drexel.edu>
2021-10-24 21:43:50 -0400

    adding tests

--- pages/api/map/index.js
+++
@@ -1,5 +0,0 @@
-export default async function handler(req, res) {
-  const response = `${process.env.REACT_APP_GOOGLE_MAPS_API_KEY}`;
-  const output = { data: response };
-  res.status(200).send(output);
-}
