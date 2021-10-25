f5a9e243b92a4193ecf94be672d81238cf1b2e7c
hg387 <hg387@drexel.edu>
2021-10-25 02:32:21 -0400

    fixed known bugs

--- config/index.js
+++ config/index.js
@@ -1,5 +1,3 @@
 const prod = process.env.NEXT_CHECK_ENV === "production"
 
-//export const server = prod ? 'https://sample-app-pvphvjlix-hg387.vercel.app' : 'http://localhost:3000'
-
-export const server = process.env.NEXT_PUBLIC_VERCEL_URL
+export const server = prod ? 'https://sample-app-pvphvjlix-hg387.vercel.app' : 'http://localhost:3000'
